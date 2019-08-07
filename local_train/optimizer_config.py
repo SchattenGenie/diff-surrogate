3f91beca192290017203235da5decfee7f01ed99
shir994 <shir994@gmail.com>
2019-08-07 22:41:59 +0000

    configs update

--- local_train/optimizer_config.py
+++ local_train/optimizer_config.py
@@ -1,13 +1,12 @@
 optimizer_config = {
-    'lr': 1.,
+    'lr': 100.,
     'num_repetitions': 5000,
-    'max_iters': 10,
-    'x_step': 1.,
-    'line_search_options': {
-        "method": 'Armijo',
-        'c0': 1.,
-        'c1': 1e-4,
-        'c2': 0.5,
-    },
+    'max_iters': 1,
+    # 'line_search_options': {
+    #     "method": 'Armijo',
+    #     'c0': 1.,
+    #     'c1': 1e-4,
+    #     'c2': 0.5,
+    # },
     'torch_model': 'RMSprop',
 }
