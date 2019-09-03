f47ad4fca10a6229f4e8a355d51eba2e1e6ac1e0
shir994 <shir994@gmail.com>
2019-09-03 20:31:02 +0000

    merge conf

--- local_train/optimizer_config.py
+++ local_train/optimizer_config.py
@@ -1,13 +1,10 @@
 optimizer_config = {
-    'lr': 5,
-    'num_repetitions': 2000,
+    'lr': 0.1,
+    'num_repetitions': 3000,
     'max_iters': 1,
-#     'x_step': 1.,
-     'line_search_options': {
-         "method": 'Wolfe',
-         'c0': 1.,
-         'c1': 1e-4,
-         'c2': 0.5,
-     },
-    'torch_model': 'RMSprop',
+#         'c0': 1.,
+#         'c1': 1e-4,
+#         'c2': 0.5,
+#     },
+#     'torch_model': 'RMSprop',
 }
