a40bcf376086f0ce47f094b906a63d08bcd52bf3
shir994 <shir994@gmail.com>
2019-09-12 22:27:19 +0000

    Implemented Cramer GAN(check). Self-attention

--- local_train/gan_config.py
+++ local_train/gan_config.py
@@ -1,14 +1,18 @@
 model_config = {
-    'task': 'WASSERSTEIN', #"REVERSED_KL",  # 'WASSERSTEIN', # WASSERSTEIN, REVERSED_KL
+    'task': "REVERSED_KL", #"CRAMER", #"REVERSED_KL",  # 'WASSERSTEIN', # WASSERSTEIN, REVERSED_KL
     'y_dim': 1,
     'x_dim': 1,
-    'psi_dim': 12,  # obsolete
+    'psi_dim': 10,  # obsolete
     'noise_dim': 100,
     'lr': 1e-4 * 4,
     'batch_size': 256,
-    'epochs': 1,
+    'epochs': 30,
     'iters_discriminator': 5,
     'iters_generator': 1,
-
-    'instance_noise_std': None
+    'instance_noise_std': None,
+    'burn_in_period': None,
+    'averaging_coeff': 0.,
+    'dis_output_dim': 1,
+    'grad_penalty': False,
+    'attention_net_size': 24
 }
--- local_train/gan_nets.py
+++ local_train/gan_nets.py
@@ -45,9 +45,29 @@ class Net(nn.Module):
         y_gen = self.fc3(h3)
         return y_gen        
 
+# Idea is kind of taken from https://arxiv.org/pdf/1805.08318.pdf and applied to conditions
+class Attention(nn.Module):
+    def __init__(self, psi_dim, hidden_dim):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self._psi_dim = psi_dim
+
+        self.f_net = nn.Conv1d(1, hidden_dim, 1, stride=1, padding=0, bias=False)
+        self.g_net = nn.Conv1d(1, hidden_dim, 1, stride=1, padding=0, bias=False)
+        self.h_net = nn.Conv1d(1, hidden_dim, 1, stride=1, padding=0, bias=False)
+        self.v_net = nn.Conv1d(hidden_dim, 1, 1, stride=1, padding=0, bias=False)
+        self.softmax = nn.Softmax(dim=2)
+        self.gamma = nn.Parameter(torch.Tensor([0.]), requires_grad=True)
+
+    def forward(self, params):
+        params_psi = params[:, :self._psi_dim].unsqueeze(1)
+        attention_map = self.softmax(torch.bmm(self.f_net(params_psi).transpose(1, 2), self.g_net(params_psi)))
+        self_attention = self.v_net(torch.bmm(attention_map, self.h_net(params_psi).transpose(1, 2)).transpose(1, 2))
+        psi_with_attention = (self.gamma * self_attention + params_psi)[:, 0, :]
+        return torch.cat([psi_with_attention, params[:, self._psi_dim:]], dim=1)
 
 class Generator(nn.Module):
-    def __init__(self, noise_dim, out_dim, psi_dim, hidden_dim=100, x_dim=1):
+    def __init__(self, noise_dim, out_dim, psi_dim, hidden_dim=100, x_dim=1, attention_net=None):
         super(Generator, self).__init__()
         
         self.fc1 = nn.Linear(noise_dim + x_dim + psi_dim, hidden_dim)
@@ -68,6 +88,7 @@ class Generator(nn.Module):
         
         # self.pc = psi_compressor
         self.psi_dim = psi_dim
+        self.attention_net = attention_net
 
     def forward(self, z, params):
         """
@@ -75,6 +96,8 @@ class Generator(nn.Module):
         """
         # psi_embedding = self.pc(params[:, :self.psi_dim])
         # z = torch.cat([z, psi_embedding, params[:, self.psi_dim:]], dim=1)
+        if self.attention_net:
+            params = self.attention_net(params)
         z = torch.cat([z, params], dim=1)
         h1 = torch.tanh(self.fc1(z))
         h4 = torch.tanh(self.fc4(h1))
@@ -88,10 +111,12 @@ class Discriminator(nn.Module):
                  in_dim,
                  psi_dim,
                  hidden_dim=100,
-                 wasserstein=False,
-                 x_dim=1):
+                 output_logits=False,
+                 x_dim=1,
+                 output_dim=1,
+                 attention_net=None):
         super(Discriminator, self).__init__()
-        self._wasserstein = wasserstein
+        self.output_logits = output_logits
 
         self.fc1 = nn.Linear(in_dim + x_dim + psi_dim, hidden_dim)
         nn.init.xavier_normal_(self.fc1.weight)
@@ -105,21 +130,24 @@ class Discriminator(nn.Module):
         nn.init.xavier_normal_(self.fc3.weight)
         nn.init.constant_(self.fc3.bias, 0.0)
         
-        self.fc4 = nn.Linear(hidden_dim, 1)
+        self.fc4 = nn.Linear(hidden_dim, output_dim)
         nn.init.xavier_normal_(self.fc4.weight)
         nn.init.constant_(self.fc4.bias, 0.0)
         
         # self.pc = psi_compressor
         self.psi_dim = psi_dim
+        self.attention_net = attention_net
 
     def forward(self, x, params):
+        if self.attention_net:
+            params = self.attention_net(params)
         x = torch.cat([x, params], dim=1)
         # psi_embedding = self.pc(params[:, :self.psi_dim])
         # x = torch.cat([x, psi_embedding, params[:, self.psi_dim:]], dim=1)
         h1 = torch.tanh(self.fc1(x))
         h2 = F.leaky_relu(self.fc2(h1))
         # h3 = F.leaky_relu(self.fc3(h2))
-        if self._wasserstein:
+        if self.output_logits:
             return self.fc4(h2)
         else:
             return torch.sigmoid(self.fc4(h2))
@@ -129,7 +157,7 @@ class GANLosses(object):
     def __init__(self, task):
         self.TASK = task
 
-    def g_loss(self, discrim_output):
+    def g_loss(self, discrim_output, discrim_output_prime=None, discrim_output_real=None):
         eps = 1e-10
         if self.TASK == 'KL':
             loss = torch.log(1 - discrim_output + eps).mean()
@@ -137,17 +165,26 @@ class GANLosses(object):
             loss = - torch.log(discrim_output + eps).mean()
         elif self.TASK == 'WASSERSTEIN':
             loss = - discrim_output.mean()
+        elif self.TASK == "CRAMER":
+            loss = (torch.norm(discrim_output_real - discrim_output, dim=1) + \
+                   torch.norm(discrim_output_real - discrim_output_prime, dim=1) - \
+                   torch.norm(discrim_output - discrim_output_prime, dim=1)).mean()
         return loss
 
-    def d_loss(self, discrim_output_gen, discrim_output_real):
+    def d_loss(self, discrim_output_gen, discrim_output_real, discrim_output_gen_prime=None):
         eps = 1e-10
         if self.TASK in ['KL', 'REVERSED_KL']:
             loss = - torch.log(discrim_output_real + eps).mean() - torch.log(1 - discrim_output_gen + eps).mean()
         elif self.TASK == 'WASSERSTEIN':
             loss = - (discrim_output_real.mean() - discrim_output_gen.mean())
+        elif self.TASK == "CRAMER":
+            loss = - (torch.norm(discrim_output_real - discrim_output_gen_prime, dim=1) -
+                      torch.norm(discrim_output_real, dim=1) -
+                      torch.norm(discrim_output_gen - discrim_output_gen_prime, dim=1) +
+                      torch.norm(discrim_output_gen_prime)).mean()
         return loss
 
-    def calc_gradient_penalty(self, discriminator, data_gen, inputs_batch, inp_data, lambda_reg=.1):
+    def calc_gradient_penalty(self, discriminator, data_gen, inputs_batch, inp_data, lambda_reg=.1, data_gen_prime=None):
         device = data_gen.device
         alpha = torch.rand(inp_data.shape[0], 1).to(device)
         dims_to_add = len(inp_data.size()) - 2
@@ -156,23 +193,29 @@ class GANLosses(object):
         # alpha = alpha.expand(inp_data.size())
 
         interpolates = (alpha * inp_data + ((1 - alpha) * data_gen)).to(device)
-
         interpolates.requires_grad_(True)
 
         disc_interpolates = discriminator(interpolates, inputs_batch)
-
-        gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                                        grad_outputs=torch.ones(disc_interpolates.size()).to(self.device),
-                                        create_graph=True, retain_graph=True, only_inputs=True)[0]
+        if self.TASK == "WASSERSTEIN":
+            gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                                            grad_outputs=torch.ones(disc_interpolates.size()).to(device),
+                                            create_graph=True, retain_graph=True, only_inputs=True)[0]
+        elif self.TASK == "CRAMER":
+            f_val = torch.norm(discriminator(data_gen_prime, inp_data) - disc_interpolates, dim=1) - \
+                    torch.norm(disc_interpolates, dim=1)
+            gradients = torch.autograd.grad(outputs=f_val, inputs=interpolates,
+                                            grad_outputs=torch.ones(f_val.size()).to(device),
+                                            create_graph=True, retain_graph=True, only_inputs=True)[0]
 
         gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * lambda_reg
         return gradient_penalty
 
     def calc_zero_centered_GP(self, discriminator, data_gen, inputs_batch, inp_data, gamma_reg=.1):
         # TODO: data_gen is not used!
+        device = inputs_batch.device
         local_input = inp_data.clone().detach().requires_grad_(True)
         disc_interpolates = discriminator(local_input, inputs_batch)
         gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=local_input,
-                                        grad_outputs=torch.ones(disc_interpolates.size()).to(self.device),
+                                        grad_outputs=torch.ones(disc_interpolates.size()).to(device),
                                         create_graph=True, retain_graph=True, only_inputs=True)[0]
         return gamma_reg / 2 * (gradients.norm(2, dim=1) ** 2).mean() 
