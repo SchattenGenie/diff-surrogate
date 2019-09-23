76886f7a10969d1a281ef8879e3e64cf45613f7a
shir994 <shir994@gmail.com>
2019-09-23 10:29:53 +0000

    Cramer GAN config

--- local_train/gan_config.py
+++ local_train/gan_config.py
@@ -3,7 +3,7 @@ model_config = {
     'y_dim': 1,  # 2
     'x_dim': 1,  # 4
     'psi_dim': 100,  # obsolete
-    'noise_dim': 300,
+    'noise_dim': 500,
     'lr': 1e-4 * 4,
     'batch_size': 256,
     'epochs': 60,
@@ -12,7 +12,7 @@ model_config = {
     'instance_noise_std': None,
     'burn_in_period': None,
     'averaging_coeff': 0.,
-    'dis_output_dim': 1,
-    'grad_penalty': False,
+    'dis_output_dim': 256,
+    'grad_penalty': True,
     'attention_net_size': None
 }
