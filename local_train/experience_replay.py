c3e5c11473eba7edc8d89e581267beb04589d6e3
SchattenGenie <belavin@phystech.edu>
2019-09-30 20:56:44 +0300

    sphere exp replay

--- local_train/experience_replay.py
+++ local_train/experience_replay.py
@@ -2,11 +2,12 @@ import torch
 
 
 class ExperienceReplay:
-    def __init__(self, psi_dim, x_dim, y_dim, device):
+    def __init__(self, psi_dim, x_dim, y_dim, device, sphere_cut=False):
         self._psi_dim = psi_dim
         self._x_dim = x_dim
         self._y_dim = y_dim
         self._device = device
+        self._sphere_cut = sphere_cut
         self._y = torch.zeros(0, self._y_dim).float().to('cpu')
         self._condition = torch.zeros(0, self._x_dim + self._psi_dim).float().to('cpu')
 
@@ -17,7 +18,10 @@ class ExperienceReplay:
 
     def extract(self, psi, step):
         psi = psi.float().to('cpu')
-        mask = ((self._condition[:, :self._psi_dim] - psi).pow(2).sum(dim=1).sqrt() < step)  # sphere
+        if self._sphere_cut:
+            mask = ((self._condition[:, :self._psi_dim] - psi).pow(2).sum(dim=1).sqrt() < step)  # sphere
+        else:
+            mask = ((self._condition[:, :self._psi_dim] - psi).abs() < step).all(dim=1)
         y = (self._y[mask]).to(self._device)
         condition = (self._condition[mask]).to(self._device)
         return y, condition
