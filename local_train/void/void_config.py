2986d386fe063d81426a4b621fc252929e1a2529
SchattenGenie <belavin@phystech.edu>
2020-03-13 18:11:43 +0300

    fixing fixed bugs

--- local_train/void/void_config.py
+++ local_train/void/void_config.py
@@ -1,2 +1,2 @@
-model_config = { 'y_dim': 1, 'psi_dim': 10, 'x_dim': 1, 'lr': 1e-2, 'K': 1, "cv_lr" : 1.}
+model_config = { 'y_dim': 1, 'psi_dim': 10, 'x_dim': 1, 'lr': 1e-2, 'K': 2, "cv_lr": 0.01}
 # in the intial implementation lr was not used at all, all opt params were default
